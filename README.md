ec12016d7f3ca95d25bc1691d3b97fafdb2f4db4
Shaon Ghosh <shaon.ghosh@ligo.org>
2021-04-23 15:33:35 -0400

    Adding README file

---
+++ README.md
@@ -0,0 +1,25 @@
+Goal of this project:
+=====================
+
+A waveform needs to be embedded on a noisy data. 
+
+
+Method:
+=======
+
+The script should do the following:
+
+[] A Python function should be there to generate a sinusoidal function given
+   a frequency, amplitude, start time, end time, attenuation function, and an
+   initial phase.
+
+[] A Python function should be there that will generate a noisy data for given
+   duration. 
+
+[] The second function should always create a data that has a duration longer 
+   than the duration of the wave.
+
+[] Reproducibility: The noise instance must be seeded, so that the exact same
+   data (signal + noise) can be generated (if desired).
+
+
--- waveform.py
+++ waveform.py
@@ -13,11 +13,17 @@ def get_time_series(f, A, t, gamma, t0, tend):
     
     # Signal time series
     T = np.linspace(t0,tend, int(((tend-t0)/t)*10000))   # Array from t0 to tend,spaced out a fraction of the # of times t is
+    # T is a numpy array that starts at t0 and ends at tend, with linearly spaced times 
+
     Y = A*np.sin(w*T + phi0)*np.exp(-gamma*T)
-    
+    # Y is the a sinusoidal displacement that lasts for the entire duration of time (t0, tend)
+
     # Carrier time series
     t = np.linspace(0, t, 10000)
+    # t is a numpy array that starts at 0 and ends at t.
+
     y = A*np.sin(w*t + phi0)*np.exp(-gamma*t)
+    # y is the displacement during this time duration t
     
     # Plotting
     pl.rcParams.update({'font.size': 18})
