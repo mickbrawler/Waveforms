a059ac6975cdfe2b2574345c8c9e21ecea366993
Michael Camilo <camilom1@montclair.edu>
2021-05-21 17:32:09 -0400

    Developed crosscorrelation function

--- CrossCorrelation.py
+++ CrossCorrelation.py
@@ -1,35 +1,32 @@
+import json
 import numpy as np
 import pylab as pl
 
 class Crosscor:
+    def __init__(self, filename):
+        with open(filename, "r") as f:
+            data = json.load(f)
+        self.dt = data["dt"]
+        self.tfull = np.array(data["t_full"])
+        self.d = np.array(data["d"])
 
-    def template(f,gamma,t0,tend):
+    def template(self, f,gamma,duration):
         
-        global t
-        global y
-        global tzero
-        global tEnd
-        
-        tzero = t0
-        tEnd = tend
-        
-        t = np.arange(t0,tend,dt) # Template time series is made in same dt as waveform
-        
-        if t[-1] != tend:         # The dt may not allow t to reach tend (Rounding issue?), add it then
-            t = np.append(t,tend)
-    
+        t = np.arange(0,duration+self.dt,self.dt) # Template time series is made in same dt as waveform
+        self.t = t        
         w = 2 * np.pi * f
-        y = np.sin(w*t)*np.exp(-gamma*t)
+        self.y = np.sin(w*t)*np.exp(-gamma*t)
         
-    # f,gamma, t0, tend
-    template(1,0,2,3)
-    
-    def match():
+    def match(self):
         
-        t_in_T = (T_full>=tzero) & (T_full<=tEnd) #t values in T_full are made TRUE (Remember T_full is same size as d)
+        ii = 0
+        match = []
         
-        usedY = d[t_in_T] # Use boolean masking on d, now d values associated with values of template times are used
-    
-        M = np.sum(usedY * y) #Multiply result with template's y, Add em
+        while len(self.d[ii:]) >= len(self.y):
+            print(len(self.d[ii:]))
+            match.append(np.sum(self.d[ii:len(self.y)] * self.y))
+            ii += 1
         
-        return(M)
+        self.match = numpy.array(match)
+
+        return(self.match)
