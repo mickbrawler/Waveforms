2f216212b6b6e9d97af1bfe33c22119eb5ab242d
mickbrawler <80130864+mickbrawler@users.noreply.github.com>
2021-05-25 22:14:25 -0400

    Added search function

--- CrossCorrelation.py
+++ CrossCorrelation.py
@@ -31,36 +31,44 @@ class Crosscor:
         self.match = np.array(match)
         self.time_slides = np.array(time_slides)
         
-        return(self.time_slides, self.match)
+        return(self.time_slides, self.match) 
+
+def search(f_low, f_hi, gamma_low, gamma_hi,duration,datafile):
+    """
+    METHOD: Takes as input the upper and lower values of frequency and gammas, constructs 
+    a bank of templates using this range of values, and then computes the maximum of the 
+    match for each templates in the bank and its corresponding time and returns that
+    """
+    f = np.arange(f_low, f_hi+1, 1)
+    g = np.arange(gamma_low, gamma_hi + 0.1, 0.1)
+
+    fs = []
+    gs = []
+    Ms = []
+    Ts = []
+
+    Obj = Crosscor(datafile)
+    for i in f:
+        for j in g:
+            Obj.template(i,j,40)
+            t,m = Obj.match()
+            M = m[np.argmax(m)] # Max match
+            T = t[np.argmax(m)] # Time associated with max match
+            fs.append(i)
+            gs.append(j)
+            Ts.append(T)
+            Ms.append(M)
+
+            output = np.vstack((fs,gs,Ts,Ms)).T
+            np.savetxt("firstattempt.txt", output, fmt="%f\t%f\t%f\t%f")
+
+            f,g,t,m = np.loadtxt("firstattempt.txt",unpack=True)
+
+    Maxm = m[np.argmax(m)]
+    Maxt = t[np.argmax(m)]
+    Maxg = g[np.argmax(m)]
+    Maxf = f[np.argmax(m)]
     
-import importlib 
+    return(Maxm,Maxt,Maxg,Maxf)
 
-f = np.arange(90,105+1,1)
-g = np.arange(0,1+.1,.1)
-
-fs = []
-gs = []
-Ms = []
-Ts = []
-
-for i in f:
-    for j in g:
-        
-        Obj = Crosscor("newdatafile.json")
-        Obj.template(i,j,40)
-        
-        M = m[np.argmax(m)] # Max match
-        T = t[np.argmax(m)] # Time associated with max match
-        fs.append(i)
-        gs.append(j)
-        Ts.append(T)
-        Ms.append(M)
-        
-output = np.vstack((fs,gs,Ts,Ms)).T
-np.savetxt("firstattempt.txt", output, fmt="%f\t%f\t%f\t%f")
-f,g,t,m = np.loadtxt("firstattempt.txt",unpack=True)
-        
-Maxm = m[np.argmax(m)]
-Maxt = t[np.argmax(m)]
-Maxg = g[np.argmax(m)]
-Maxf = f[np.argmax(m)]
+search(90,105,0,1,"newdatafile.json")
