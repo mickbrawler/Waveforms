d33e90c8de650cbad60c048787dc78c222b0f77d
Michael Camilo <80130864+mickbrawler@users.noreply.github.com>
2021-07-09 16:15:57 -0400

    .py file ready for high # threshold ROC Curve runs

--- Statistic_Analysis.py
+++ Statistic_Analysis.py
@@ -6,7 +6,7 @@ import math
 import time
 import operator
 
-def waveforms(N_A, N_g, N_f, t0_tf, T, B, trials, seedn = 1, inputfile="input", phi0=0, A0=1, 
+def waveforms(N_A, N_g, N_f, t0_tf, T, B, trials, seedn=1, inputfile="input", phi0=0, A0=1, 
               Af=50, g0=0, gf=2, F0=90, Ff=110, N=1000):
     """
     METHOD
@@ -42,18 +42,18 @@ def waveforms(N_A, N_g, N_f, t0_tf, T, B, trials, seedn = 1, inputfile="input", 
     A_RANGE=np.linspace(A0,Af,N_A)
     G_RANGE=np.linspace(g0,gf,N_g)
     F_RANGE=np.linspace(F0,Ff,N_f)
-    
+
     # number of parameters available
-    A_LEN, G_LEN, F_LEN = len(A_RANGE), len(G_RANGE), len(F_RANGE)
+    A_LEN, G_LEN, F_LEN = N_A, N_g, N_f
     
     waveform_data={}
     for j in range(trials):
         waveform_data.update({j:[[],[]]})
         
         # calculates random indice for each parameter (A, f, g)
-        A_RAN=np.random.randint(0,A_LEN)
-        G_RAN=np.random.randint(0,G_LEN)
-        F_RAN=np.random.randint(0,F_LEN)
+        A_RAN=random.randint(0,A_LEN-1)
+        G_RAN=random.randint(0,G_LEN-1)
+        F_RAN=random.randint(0,F_LEN-1)
         
         # calculates random parameters A, f, g
         A, gamma, f = A_RANGE[A_RAN], G_RANGE[G_RAN], F_RANGE[F_RAN]
@@ -65,16 +65,16 @@ def waveforms(N_A, N_g, N_f, t0_tf, T, B, trials, seedn = 1, inputfile="input", 
 
         ##NOTE: using 't0' instead of some multiple of dt may cause issues later
         
-        SIG_LEN =(math.floor(t0_tf/dt)+1 if (t0_tf != T) else N) # calculate # of indexes signal takes
+        SIG_LEN = (math.floor(t0_tf/dt)+1 if (t0_tf != T) else N) # calculate # of indexes signal takes
         INJECTED = np.zeros(N)                 # initalize injected signal, with N size array of zeroes
         for i in range(SIG_LEN):
-            INJECTED[ START_INDEX + i]=t0+i*dt       # fill in injected signal with its time stamps
+            INJECTED[START_INDEX + i]=t0+i*dt       # fill in injected signal with its time stamps
 
         w = 2 * np.pi * f
         
         # replace timestamps with their displacement values
         SR = INJECTED[START_INDEX : START_INDEX+SIG_LEN][:]
-        INJECTED[START_INDEX : START_INDEX+SIG_LEN] = A*np.sin(w*(SR) + phi0)*np.exp(-gamma*( SR-t0))
+        INJECTED[START_INDEX : START_INDEX+SIG_LEN] = A*np.sin(w*(SR) + phi0)*np.exp(-gamma*(SR-t0))
         
         # Purposed for Quadrature Sum
         D_i = [] # list of each differently seeded waveform
@@ -86,10 +86,10 @@ def waveforms(N_A, N_g, N_f, t0_tf, T, B, trials, seedn = 1, inputfile="input", 
         # gets parameters and data for each trial, stuffs it into dictionary
         parameters = [A, f, gamma, t0]
         waveform_data[j][0], waveform_data[j][1] = parameters, D_i
-    
+        
     # each trial has list of parameters used and list of data values
-    with open("Stats/waveform_data/{}-waveform_data.json".format(inputfile) , "w") as f:
-        json.dump(waveform_data, f, indent=2, sort_keys=True)
+    with open("{}-waveform_data.json".format(inputfile) , "w") as f:
+        json.dump(waveform_data, f, indent=2, sort_keys=True)        
 
 class OnSource:
     
@@ -154,7 +154,7 @@ class OnSource:
                        for g in self.G_RANGE for f in self.F_RANGE]
         
         # NEW ADDITION: Reads waveform data file 
-        with open("Stats/waveform_data/{}-waveform_data.json".format(inputfile),"r") as f: 
+        with open("{}-waveform_data.json".format(inputfile),"r") as f: 
             waveform_data = json.load(f)
         
         self.waveform_data = waveform_data
@@ -204,7 +204,7 @@ class OnSource:
         self.output = output
         
         # saves 'output' as a .json in current working directory.
-        '''with open("Stats/waveform_data/{}-stat_data.json".format(outputfile), "w") as f:
+        '''with open("{}.json".format(outputfile), "w") as f:
             json.dump(output, f, indent=2, sort_keys=True)'''
     
     # calculates test statistic, stores it internally, and returns a copy of it as a dictionary 
@@ -219,7 +219,6 @@ class OnSource:
     
         # initalizes rho statistic dictionary
         self.RHO = {}
-    
         # indexed to loops through dataset trials
         for i in range(self.trials):
             rho_i = []
@@ -232,8 +231,8 @@ class OnSource:
                 
                 # NEW ADDITION: Evaluates string (Exec gave issues... eval is the same concept though)
                 p = eval(stat)
-                rho_i.append(list(p))
-            
+                listp = list(p)
+                rho_i.append(listp)
             self.RHO.update({ i : rho_i })
         
         self.stat = stat
@@ -270,7 +269,7 @@ class OnSource:
         if (2*D >= self.dt):
             
             self.RHO_MOD = {}
-        
+            find_thresh = []
             for i in range(self.trials):
 
                 # gets the length
@@ -286,36 +285,22 @@ class OnSource:
                     BG_VALS_IJ.append(self.modulator( self.RHO[i][j][:],D))
 
                     FG_VAL_IJ.append(BG_VALS_IJ[j].pop(T0_2D))
-                
-                self.RHO_MOD.update({ i: [ BG_VALS_IJ, FG_VAL_IJ ] })
-        
+                    
+                find_thresh += FG_VAL_IJ # 1d list of all forground values 
+                self.RHO_MOD.update({ i: [ BG_VALS_IJ, FG_VAL_IJ ] }) # these are the peaks we look for
+            
+            self.pot_thresh0 = min(find_thresh) # Used to set thresholds in ROC Curve
+            self.pot_threshf = max(find_thresh)
             return self.RHO_MOD
         
         else: 
             print("invalid D; it is required that 2*D >= T/N")
 
-            
 # test_plotter and scatter_plotter may be combinable by using arguments to trigger the creation of specific graphs
 # that is if we are always gonna be making these scatter plots
 
-    def test_plotter(self, T, N, new_stat_only=True, index=0, output="test"):
-        
-        mapping = {}
-        
-        # Creates three dimensional array size of potential paramaters available to each data
-        holder = np.full((self.F_LEN, self.A_LEN, self.G_LEN), 0, dtype=list)
-
-        for j in range(self.F_LEN):
-            for i in range(self.A_LEN):
-                for k in range(self.G_LEN):
-                    A=self.A_RANGE[i]
-                    F=self.F_RANGE[j]
-                    G=self.G_RANGE[k]
-            
-                    holder[(i,j,k)] = [0]
-                    mapping.update({ (A,F,G) : holder[(i,j,k)]})
-                    holder[(i,j,k)] = holder[(i,j,k)][0]
-        
+    def test_plotter(self, T0, Tf, N, new_stat_only=True, index=0, outputfile="test"):
+    
         # initalizes threshold axis points, counts for each threshold
         # and space of parameter space
         THRS_AXIS = []
@@ -329,18 +314,15 @@ class OnSource:
 
         # sets current threshold value
         # NEW ADDITION: thrshld will need to actually decrease in order to find smallest chi-square
-        thrshlds = np.linspace(0,T,N)
+        thrshlds = np.linspace(T0,Tf,N)
         oper = operator.gt
         
         # NEW ADDITION: Threshold operations are altered accordingly
         if self.stat == "CS_IJ":
-            thrshlds = np.linspace(0,T,N)[::-1]
+            thrshlds = np.linspace(T0,Tf,N)[::-1]
             oper = operator.lt
-            
+        
         for thrshld in thrshlds:
-
-            THRS_AXIS.append(thrshld)
-            succ_count_thrhld = 0
 
             # NEW ADDITION: Detection/ False Alarm probability counters
             Detect_count = 0
@@ -349,29 +331,21 @@ class OnSource:
             #loops from trial/ parameter space pairs 
             for i in range(self.trials):
                 heat_count = 0
-
+                
+                # Detection Probability
+                Max_FG_ij = max(self.RHO_MOD[i][1]) # max of onsources per trial
+                if oper(Max_FG_ij,thrshld):
+                    Detect_count += 1
+                    
                 for j in range(PSPACE_LEN):
 
                     RM_ij = np.array(self.RHO_MOD[i][0][j])
                     FG_ij = self.RHO_MOD[i][1][j]
                     N_BG_ij = len(RM_ij[RM_ij > FG_ij])
                     
-                    # NEW ADDITION: Detection probability
-                    if oper(FG_ij,thrshld):
-                        Detect_count += 1
-
                     # NEW ADDITION: False Alarm probability
                     falses_ij = len(RM_ij[oper(RM_ij,thrshld)])
                     False_count += falses_ij
-
-                    # tests if the given pair passes the threshold test
-                    if ((N_BG_ij==0) and (FG_ij > thrshld)):
-                        if heat_count == 0:
-                            succ_count_thrhld += 1
-                            mapping[tuple(self.output[i][0][:3])][0] += 1
-                            heat_count += 1
-
-            COUNT_AXIS.append(succ_count_thrhld)
 
             # NEW ADDITION: Detection/False Alarm probability stats
             Detect_stat = Detect_count / self.trials
@@ -380,90 +354,14 @@ class OnSource:
             # NEW ADDITION: Appending stat per threshold
             self.Detection_Prob.append(Detect_stat)
             self.New_False_Prob.append(False_stat)
-        
-        if new_stat_only == False:
-            
-            plt.plot(THRS_AXIS,COUNT_AXIS)
-            plt.xlabel("$Threshold$")
-            plt.ylabel("Counts")
-            plt.show()
-            plt.savefig("Stats/stat_plots/{}_counts.png".format(outputfile))
 
-            # Don't quite get this last part of test_plotter
-            w = holder.sum(index)
-
-            cop = np.full(w.shape,0)
-            z = [(i,j) for i in range(w.shape[0]) for j in range(w.shape[1])]
-            for tup in z:
-                cop[tup] = int(w[tup])
-            
-            plt.imshow(cop, cmap=plt.cm.hot) 
-            plt.savefig("Stats/stat_plots/{}_heat.png".format(outputfile))
-
-#             plt.imshow(heat_array, cmap=plt.cm.hot) 
-    
-    def Scatter_plotter(self, thrshld, xvar, yvar, outputfile="test"): 
-    
-        label = ["frequency", "amplitude", "gamma"]
-        PSPACE_LEN = len(self.AGF_PAIR)
-        SUCC_PAIRS = ([],[])
-        FAIL_PAIRS = ([],[])
-
-        #loops from trial/ parameter space pairs 
-        for i in range(self.trials):
-        
-            succ_count_thrhld = 0  # test condition for adding to fail array
-
-            for j in range(PSPACE_LEN):
-            
-                RM_ij = np.array(self.RHO_MOD[i][0][j])  # get moded rho_ij background values
-                FG_ij = self.RHO_MOD[i][1][j]            # get moded rho_ij foreground value
-                N_BG_ij = len(RM_ij[ RM_ij > FG_ij])     # get background vals > froeground vals
-                
-                # tests if the given pair passes the threshold test
-                if ((N_BG_ij==0) and (FG_ij>thrshld)):
-                
-                    parameter_x=self.output[i][0][xvar]  # get parameter 'xvar' of trial i
-                    parameter_y=self.output[i][0][yvar]  # get parameter 'yvar' of trial i
-                    SUCC_PAIRS[0].append(parameter_x) # add to successes
-                    SUCC_PAIRS[1].append(parameter_y)
-                    succ_count_thrhld+=1
-                    break
-        
-            # sees if trial i failed the test; adds parameters to fail array
-            if succ_count_thrhld==0:
-                parameter_x=self.output[i][0][xvar]
-                parameter_y=self.output[i][0][yvar]
-                FAIL_PAIRS[0].append(parameter_x)
-                FAIL_PAIRS[1].append(parameter_y)
-    
-        colors = ("red", "blue")
-        groups = ("pass", "fail")
-        marks = ("o", "*")
-        datas = (SUCC_PAIRS, FAIL_PAIRS)
-    
-        # Create plot
-        fig = plt.figure()
-        ax = fig.add_subplot(1, 1, 1)
-    
-        for data, marks, color, group in zip(datas, marks, colors, groups):
-            x, y = data
-            ax.scatter(x, y, marker=marks, c=color, edgecolors='none', s=30, label=group)
-
-        plt.title('found-missed plot')
-        plt.xlabel(label[xvar])
-        plt.ylabel(label[yvar])
-        plt.legend(loc=2)
-        plt.savefig("Stats/stat_plots/{}_scatter.png".format(outputfile))
-        plt.show()
-    
     def ROC_Curve(self, n_s, N, new_stat_only=True, outputfile="ROC_test"):
         
         # Choose soley n-rho's or an addition stat
         if new_stat_only == True:
             stat_list = []
         else:
-            stat_list = ["CC_IJ","CS_IJ"]
+            stat_list = ["CC_IJ"]
             
         # different ^n rhos are appended
         for n in range(1,n_s+1):
@@ -471,27 +369,18 @@ class OnSource:
             stat_list.append(rho)
         
         stat_length = len(stat_list)
-        defaultT = .1
         for s in range(stat_length):
-            if stat_list[s] == "CC_IJ":
-                T = 1000 # CC_IJ 10000
-            elif stat_list[s] == "CS_IJ":
-                T = 100000
-
-            else:
-                T = defaultT
-                defaultT = defaultT * (10 ** -3)
-                
+    
             self.rho(stat = stat_list[s])
             self.window(.02)
-            self.test_plotter(T, N)
-            plt.plot(self.New_False_Prob,self.Detection_Prob, label=stat_list[s])
+            self.test_plotter(self.pot_thresh0, self.pot_threshf, N)
+            plt.plot(self.New_False_Prob, self.Detection_Prob, label=stat_list[s])
             
         plt.xlabel("New_False_Probs")
         plt.ylabel("Detection_Probs")
-        plt.title("ROC Curve: n's={}: N={}".format(n_s,N))
+        plt.title("ROC Curve:n's={}:N={}".format(n_s,N))
         plt.legend()
-        plt.savefig("Stats/stat_plots/{}-ROC.png".format(outputfile))
+        plt.savefig("{}-ROC.png".format(outputfile))
         plt.show()
 
     # Produces a template given a position in parameter space
