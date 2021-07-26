bd0eee2fd463e9e4504a9c2bcfb3f81185e4b3c0
Michael Camilo <80130864+mickbrawler@users.noreply.github.com>
2021-07-25 23:39:56 -0400

    Create heatmap.py

---
+++ heatmap.py
@@ -0,0 +1,139 @@
+import json
+import numpy as np
+from matplotlib import pyplot as plt
+%matplotlib inline
+import math
+import time
+
+def heatmap(var1, var2, T, max_OS=-1, max_BG=-1, stat=2, plot1="heat_plot1", plot2 = "heat_plot2"):
+
+    with open("run_uniques/essentials.json", "r") as f:
+        essentials = json.load(f)
+    A_LEN = essentials["essentials"][0]
+    F_LEN = essentials["essentials"][1]
+    G_LEN = essentials["essentials"][2]
+    F_RANGE = essentials["essentials"][3]
+    A_RANGE = essentials["essentials"][4]
+    G_RANGE = essentials["essentials"][5]
+    AFG_PAIR = essentials["essentials"][6]
+    trials = essentials["essentials"][7]
+
+    with open("Merged_jsons/Merged_output.json", "r") as f:
+        output = json.load(f)
+    
+    with open("Merged_jsons/Merged_Max_OS.json", "r") as f:
+        MAX_OS = json.load(f)
+
+    with open("Merged_jsons/Merged_Max_BG_TEMP.json", "r") as f:
+        MAX_BG_TEMP = json.load(f)
+    
+    index=index_counter(var1,var2)
+
+    mapping={}   #initalizes mapping for heat map
+    count_mapping={} #initalies array to count occurances of certain parameters
+    BG_mapping={}
+
+    holder=np.full((A_LEN, F_LEN, G_LEN), 0, dtype=list)
+    count_holder=np.full((A_LEN, F_LEN, G_LEN), 0, dtype=list)
+    BG_holder=np.full((A_LEN, F_LEN, G_LEN), 0, dtype=list)
+
+    # builds link between keys and np-array 
+    for j in range(F_LEN):
+        for i in range(A_LEN):
+            for k in range(G_LEN):
+                F=F_RANGE[j]
+                A=A_RANGE[i]
+                G=G_RANGE[k]
+
+                holder[(i,j,k)]=[0]
+                count_holder[(i,j,k)]=[0]
+                BG_holder[(i,j,k)]=[0]
+
+                mapping.update({ (A,F,G) : holder[(i,j,k)]})
+                count_mapping.update({ (A,F,G) : count_holder[(i,j,k)]})
+                BG_mapping.update({ (A,F,G) : BG_holder[(i,j,k)]})
+
+    #form BG mappings
+    num_j=0
+    for j in AFG_PAIR:
+        BG_mapping[tuple(j)][0]=MAX_BG_TEMP[str(num_j)][stat]
+        num_j+=1
+
+    # form OS mappings
+    for OS in MAX_OS.values():
+        if (T<=OS[str(stat)][1]):
+            mapping[tuple(OS[str(stat)][0])][0]+=float(OS[str(stat)][1])
+            count_mapping[tuple(OS[str(stat)][0])][0]+=1
+
+    #redefines things to be normal arrays inside 
+    for j in range(F_LEN):
+        for i in range(A_LEN):
+            for k in range(G_LEN):
+                holder[(i,j,k)]=holder[(i,j,k)][0]
+                count_holder[(i,j,k)]=count_holder[(i,j,k)][0]
+                BG_holder[(i,j,k)]=BG_holder[(i,j,k)][0]
+
+    #marginalizes array in direction of index
+    index=int(index)
+    plot_array=holder.sum(index)
+    normalizer=count_holder.sum(index)
+    BG_plot=BG_holder.sum(index)
+
+    #builds a copy of the original array, but fixes things to be integers 
+    pa_cop=np.full(plot_array.shape,0.0)
+    n_cop=np.full(normalizer.shape,0.0)
+    BG_cop=np.full(BG_plot.shape,0.0)
+    z=[(i,j) for i in range(plot_array.shape[0]) for j in range(plot_array.shape[1])]
+    for tup in z:
+        pa_cop[tup]=plot_array[tup]
+        n_cop[tup]=normalizer[tup]
+        BG_cop[tup]=BG_plot[tup]
+
+    #normalize
+    pa_cop[n_cop>0]=pa_cop[n_cop>0]/n_cop[n_cop>0]
+
+    if (max_OS==-1):
+        vmax_val=pa_cop.max()
+    else:
+        vmax_val=max_OS
+
+    label=np.array(["A", "F", "G"])
+    IL=np.array([0, 1, 2])
+    IL=IL[ IL != index]
+
+#     XV="self."+label[IL[1]]+"_RANGE"
+    XV=label[IL[1]]+"_RANGE"
+    XA=eval(XV)
+
+#     YV="self."+label[IL[0]]+"_RANGE"
+    YV=label[IL[0]]+"_RANGE"
+    YA=eval(YV)
+
+    contours = plt.contour(XA, YA, pa_cop, 10, colors='blue')
+    plt.clabel(contours, inline=True, fontsize=8)
+
+    plt.contourf(XA, YA, pa_cop, 100, cmap='hot', alpha=1, vmin=T, vmax=vmax_val);
+    plt.colorbar();
+
+    plt.xlabel(label[IL[1]])
+    plt.ylabel(label[IL[0]])
+    plt.title('Max On Source; t='+str(T))
+    plt.show()
+    plt.savefig("plots/{}".format(plot1))
+    
+    if (max_BG==-1):
+        vmax_val=BG_cop.max()
+    else:
+        vmax_val=max_BG
+
+    contours = plt.contour(XA, YA, BG_cop, 10, colors='blue')
+    plt.clabel(contours, inline=True, fontsize=8)
+
+    plt.contourf(XA, YA, BG_cop, 100, cmap='hot', alpha=1, vmin=T, vmax=vmax_val);
+    plt.colorbar();
+
+    plt.xlabel(label[IL[1]])
+    plt.ylabel(label[IL[0]])
+    plt.title('Max Background; t='+str(T))
+    plt.show()
+    plt.savefig("plots/{}".format(plot2))
